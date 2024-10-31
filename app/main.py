5ee8572aad9758799b6817674e633d1d6800f84c
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2024-10-31 12:05:49 +0100

    implemented -class Animal -class Herbivore -class Carnivore

--- app/main.py
+++ app/main.py
@@ -1,1 +1,26 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name, health=100, hidden=False):
+        self.name = name
+        self.health = health
+        self.hidden = hidden
+        Animal.alive.append(self)
+
+    def die(self):
+        if self in Animal.alive:
+            Animal.alive.remove(self)
+
+    def __repr__(self):
+        return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
+
+class Herbivore(Animal):
+    def hide(self):
+        self.hidden = not self.hidden
+
+class Carnivore(Animal):
+    def bite(self, animal):
+        if isinstance(animal, Herbivore) and not animal.hidden:
+            animal.health -= 50
+            if animal.health <= 0:
+                animal.die()
